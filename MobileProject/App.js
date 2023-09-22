89291bfb1b1e7b5285b0385cd082cb30334075f9
Squarehead2 <118778307+Squarehead2@users.noreply.github.com>
2023-09-22 11:26:32 -0600

    new fonts

--- MobileProject/App.js
+++ MobileProject/App.js
@@ -1,10 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import { useFonts } from "expo-font";
+import * as React from "react";
 
 export default function App() {
+  const [loaded] = useFonts({
+    "challenger-font": require("./assets/fonts/ChallengerROUGH.ttf"),
+  });
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.header}>WACKY WEATHER</Text>
+      <TextInput style={styles.input} placeholder="Enter City Name" />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +23,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#42adf5",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    flex: 1,
+    fontSize: 55,
+    paddingTop: 70,
+    fontFamily: "challenger-font",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    flex: 1,
+    fontSize: 25,
+    paddingTop: 70,
+    fontFamily: "challenger-font",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
