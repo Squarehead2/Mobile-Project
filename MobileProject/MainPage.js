c3f3c6db266bc75fe75e7bb77598400d64f2d1b6
Paul <123585529+Duncan2234587799@users.noreply.github.com>
2023-11-27 20:21:33 -0700

    weird

--- MobileProject/MainPage.js
+++ MobileProject/MainPage.js
@@ -149,7 +149,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "rgb(49, 158, 232)",
     alignItems: "center",
-    justifyContent: 1,
+    justifyContent: 'center',
   },
   location: {
     fontSize: 18,
