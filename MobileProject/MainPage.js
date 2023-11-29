40b7a2d97b158778b6db9963603a20f8e24af4b9
Paul <123585529+Duncan2234587799@users.noreply.github.com>
2023-11-28 18:34:43 -0700

    Back to saquare 1

--- MobileProject/MainPage.js
+++ MobileProject/MainPage.js
@@ -3,11 +3,14 @@ import {
   StyleSheet,
   Text,
   View,
+  TextInput,
   Image,
   ImageBackground,
   ScrollView,
   SafeAreaView,
+  FlatList,
   Alert,
+  RefreshControl,
   ActivityIndicator,
 } from "react-native";
 import { useFonts } from "expo-font";
@@ -17,7 +20,6 @@ export default function MainPage() {
   const [forecast, setForecast] = useState(null);
   const [loading, setLoading] = useState(true);
   const [location, setLocation] = useState(null);
-  const [weatherIcon, setWeatherIcon] = useState(null);
 
   useEffect(() => {
     (async () => {
@@ -27,22 +29,10 @@ export default function MainPage() {
         return;
       }
 
-      try {
-        let loc = await Location.getCurrentPositionAsync({});
-        setLocation(loc);
-      } catch (error) {
-        console.error("Error getting location:", error);
-      }
+      let loc = await Location.getCurrentPositionAsync({});
+      setLocation(loc);
     })();
   }, []);
-
-  useEffect(() => {
-    if (forecast) {
-      const dataIcon = forecast.weather[0].icon;
-      const weatherIconUrl = `http://openweathermap.org/img/wn/${dataIcon}.png`;
-      setWeatherIcon(weatherIconUrl);
-    }
-  }, [forecast]);
 
   useEffect(() => {
     if (location) {
@@ -95,13 +85,58 @@ export default function MainPage() {
         style={styles.cloudgif}
       />
       <ScrollView>
-        {/* ... rest of your code */}
+        <Text style={styles.header}>Today's Forecast</Text>
+        <Text style={styles.location}>
+          {forecast.name}, {forecast.sys.country}
+        </Text>
+        <Text style={styles.temp}>{forecast.main.temp}°C</Text>
+        <Text style={styles.description}>
+          {forecast.weather[0].description}
+        </Text>
+        <Text style={styles.feelslike}>
+          Feels like {forecast.main.feels_like}°C
+        </Text>
+        <Text style={styles.humidity}>Humidity: {forecast.main.humidity}%</Text>
+        <Text style={styles.wind}>Wind: {forecast.wind.speed} km/h</Text>
+        <Text style={styles.pressure}>
+          Pressure: {forecast.main.pressure} hPa
+        </Text>
+        <Text style={styles.visibility}>
+          Visibility: {forecast.visibility / 1000} km
+        </Text>
+        <Text style={styles.sunrise}>
+          Sunrise:{" "}
+          {new Date(forecast.sys.sunrise * 1000).toLocaleTimeString("en-US")}
+        </Text>
+        <Text style={styles.sunset}>
+          Sunset:{" "}
+          {new Date(forecast.sys.sunset * 1000).toLocaleTimeString("en-US")}
+        </Text>
+        <Text style={styles.timezone}>
+          Timezone: {forecast.timezone / 3600} GMT
+        </Text>
+        <Text style={styles.date}>
+          {new Date().toLocaleDateString("en-US", {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}
+        </Text>
+        <Text style={styles.time}>
+          {new Date().toLocaleTimeString("en-US", {
+            hour: "numeric",
+            minute: "numeric",
+          })}
+        </Text>
+        <Image
+          source={require("./assets/cloudsshort.png")}
+          style={styles.navImage}
+        />
       </ScrollView>
     </SafeAreaView>
   );
 }
-
-
 
 const styles = StyleSheet.create({
   loading: {
@@ -222,12 +257,5 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     fontFamily: "challenger-font",
     fontSize: 16,
-  },
-  weatherIcon:{
-    width: 50,
-    height: 50,
-    alignSelf:'center',
-    marginTop: 20,
-    marginBottom: 20,
   },
 });
